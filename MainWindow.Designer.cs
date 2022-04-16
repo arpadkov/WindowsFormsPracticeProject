db7ff8fc602c4ec879f919933107166a9c917432
95arp <95.arpadkov@gmail.com>
2022-04-16 17:25:13 +0200

    added ScottPlot

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsPracticeProject
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.plotButton = new System.Windows.Forms.Button();
+            this.MousePositionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(591, 92);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(314, 46);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "Click This!";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Location = new System.Drawing.Point(817, 218);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(35, 13);
+            this.lblHelloWorld.TabIndex = 1;
+            this.lblHelloWorld.Text = "label1";
+            this.lblHelloWorld.Click += new System.EventHandler(this.lblHelloWorld_Click);
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Location = new System.Drawing.Point(52, 76);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(483, 359);
+            this.formsPlot1.TabIndex = 2;
+            this.formsPlot1.Load += new System.EventHandler(this.formsPlot1_Load);
+            this.formsPlot1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.formsPlot1_MouseMoved);
+            // 
+            // plotButton
+            // 
+            this.plotButton.ForeColor = System.Drawing.Color.Black;
+            this.plotButton.Location = new System.Drawing.Point(616, 305);
+            this.plotButton.Name = "plotButton";
+            this.plotButton.Size = new System.Drawing.Size(75, 23);
+            this.plotButton.TabIndex = 3;
+            this.plotButton.Text = "Plot!";
+            this.plotButton.UseVisualStyleBackColor = true;
+            this.plotButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // MousePositionLabel
+            // 
+            this.MousePositionLabel.AutoSize = true;
+            this.MousePositionLabel.Location = new System.Drawing.Point(806, 421);
+            this.MousePositionLabel.Name = "MousePositionLabel";
+            this.MousePositionLabel.Size = new System.Drawing.Size(56, 13);
+            this.MousePositionLabel.TabIndex = 4;
+            this.MousePositionLabel.Text = "No Mouse";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 516);
+            this.Controls.Add(this.MousePositionLabel);
+            this.Controls.Add(this.plotButton);
+            this.Controls.Add(this.formsPlot1);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.btnClickThis);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label lblHelloWorld;
+        private ScottPlot.FormsPlot formsPlot1;
+        private System.Windows.Forms.Button plotButton;
+        private System.Windows.Forms.Label MousePositionLabel;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsPracticeProject
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World!";
+        }
+
+        private void lblHelloWorld_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formsPlot1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formsPlot1_MouseMoved(object sender, MouseEventArgs e)
+        {
+            (double x, double y) = formsPlot1.GetMouseCoordinates();
+            MousePositionLabel.Text = String.Format("Mouse Position: {0} - {1}", x, y);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            double[] values = { 26, 20, 23, 7, 16 };
+            formsPlot1.Plot.AddBar(values);
+            formsPlot1.Plot.Title("MyPlot");
+
+            formsPlot1.Refresh();
+        }
+    }
+}
